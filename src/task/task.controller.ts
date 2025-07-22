3664f5ab4c6755e005ea3914d3bfc2d808c0b744
Hetor17 <hemr17@hotmail.com>
2025-07-22 12:51:19 -0700

    Hola

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,8 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { Task } from 'generated/prisma';
 
-@Controller('task')
+@Controller()
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @Get()
+  async getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get()
+  async getTask(@Body() id: number) {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(data: Task){
+    return this.taskService.createTask(data);
+  }
+
+  @Put()
+  updateTask(id: number, data: Task) {
+    return this.taskService.updateTask(id, data);
+  }
+
+  @Delete()
+  deleteTask(id:number){
+    return this.taskService.deleteTask(id);
+  }
 }
